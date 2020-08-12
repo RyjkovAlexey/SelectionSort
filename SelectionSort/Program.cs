e9cda7756b92b2070181d59157de7b90e733a6cb
AlexeyRyjkov <kawai4ik98@gmail.com>
2020-08-12 13:29:35 +0300

    Реализация сортировки выбором

---
+++ SelectionSort/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = {5,16,10,11,25,15,30};
+            SelectionSort.Sort(array);
+            string str = "";
+            for (int i = 0; i < array.Length; i++) str += $" {array[i]}";
+            Console.WriteLine(str);
+        }
+    }
+}
